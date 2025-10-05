99acc9bfcf1326a006b4eef8fe35dff044dd4be2
tashaxanavsem82 <swgwull@hotmail.com>
2025-10-05 12:50:12 -0500

    Update welcome message for clarity.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Team Sync Hub!');
+  res.send('Welcome to the Team Sync Hub - your collaboration starts here!');
 });
 
 app.listen(PORT, () => {
